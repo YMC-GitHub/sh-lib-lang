1865c4ebcc02d3cc850c04594391527773f6acc0
yemiancheng <ymc.github@gmail.com>
2019-08-14 08:08:08 +0800

    test(core): support multi language

--- test/index.sh
+++ test/index.sh
@@ -14,15 +14,76 @@ source "$SRC_PATH/index.sh"
 
 recordTime "lang.curd.cost"
 
+function test_unit(){
+echo "-------test.unit.lang_type_get/set-------"
+test 'lang_type_get' "zh"
+lang_type_set "en"
+test 'lang_type_get' "en"
+lang_type_set "zh"
+test 'lang_type_get' "zh"
+echo "-------test.unit.lang_type_get/set-------"
+echo
+
+
+echo "-------test.unit.lang_step_set/get-------"
+desc=
+lang_step_set "$desc"
+test 'lang_step_now "1"' ""
+desc=$(cat <<EOF
+# 创建主机地址文件
+# 测试主机是否在线
+EOF
+)
+lang_step_set "$desc"
+test 'lang_step_now "1"' "# 创建主机地址文件"
+echo "-------test.unit.lang_step_set/get-------"
+echo
+
+
+}
+function test_e2e(){
+
+echo "-------test.e2e.lang-------"
+desc=$(cat <<EOF
+# 创建主机地址文件
+# 测试主机是否在线
+EOF
+)
+lang_step_set "$desc"
+test 'lang_type_get' "zh"
+test 'lang_step_now "1"' "# 创建主机地址文件"
+test 'lang_step_now "2"' "# 测试主机是否在线"
+
 desc=$(cat <<EOF
 # step 1
 # step 2
 EOF
 )
-lang_step_set "$desc"
+lang_step_set "$desc" "en"
 
+lang_type_set "en"
+test 'lang_type_get' "en"
 test 'lang_step_now "1"' "# step 1"
 test 'lang_step_now "2"' "# step 2"
+
+lang_type_set "zh"
+test 'lang_type_get' "zh"
+test 'lang_step_now "1"' "# 创建主机地址文件"
+test 'lang_step_now "2"' "# 测试主机是否在线"
+echo "-------test.e2e.lang-------"
+}
+
+function test_main(){
+  test_unit
+  test_e2e
+}
+#test_unit
+#test_e2e
+test_main
+#lang_sample
+
+#echo ${lang_step_dic[@]}
+
 caculateTime "lang.curd.cost"
 
 ## file-usage
